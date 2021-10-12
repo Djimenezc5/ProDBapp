54d731d9f4cc49f32d8bf8913130cedc1c5620f1
HP <HP@LAPTOP-TNRTV2NU>
2021-10-12 12:04:14 -0600

    cambios menores

---
+++ PDBApp/Actualizacion_proveedores.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace PDBApp
+{
+    partial class Actualizacion_proveedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttClose = new System.Windows.Forms.Button();
+            this.tbCorreo = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.cbDep = new System.Windows.Forms.ComboBox();
+            this.tbDirRes = new System.Windows.Forms.TextBox();
+            this.tbTelResidencial = new System.Windows.Forms.TextBox();
+            this.tbTelCelular = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.bttAceptar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labeRE = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttClose
+            // 
+            this.bttClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(25)))), ((int)(((byte)(42)))));
+            this.bttClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttClose.ForeColor = System.Drawing.Color.LightGray;
+            this.bttClose.Location = new System.Drawing.Point(594, 520);
+            this.bttClose.Margin = new System.Windows.Forms.Padding(4);
+            this.bttClose.Name = "bttClose";
+            this.bttClose.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.bttClose.Size = new System.Drawing.Size(78, 30);
+            this.bttClose.TabIndex = 90;
+            this.bttClose.Text = "Close";
+            this.bttClose.UseVisualStyleBackColor = true;
+            // 
+            // tbCorreo
+            // 
+            this.tbCorreo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbCorreo.Location = new System.Drawing.Point(367, 257);
+            this.tbCorreo.Name = "tbCorreo";
+            this.tbCorreo.Size = new System.Drawing.Size(229, 20);
+            this.tbCorreo.TabIndex = 89;
+            // 
+            // label12
+            // 
+            this.label12.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.LightGray;
+            this.label12.Location = new System.Drawing.Point(197, 257);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(158, 20);
+            this.label12.TabIndex = 88;
+            this.label12.Text = "Correo Electrónico";
+            // 
+            // cbDep
+            // 
+            this.cbDep.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbDep.FormattingEnabled = true;
+            this.cbDep.Location = new System.Drawing.Point(367, 216);
+            this.cbDep.Name = "cbDep";
+            this.cbDep.Size = new System.Drawing.Size(229, 21);
+            this.cbDep.TabIndex = 87;
+            // 
+            // tbDirRes
+            // 
+            this.tbDirRes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbDirRes.Location = new System.Drawing.Point(367, 297);
+            this.tbDirRes.Name = "tbDirRes";
+            this.tbDirRes.Size = new System.Drawing.Size(229, 20);
+            this.tbDirRes.TabIndex = 85;
+            // 
+            // tbTelResidencial
+            // 
+            this.tbTelResidencial.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbTelResidencial.Location = new System.Drawing.Point(367, 173);
+            this.tbTelResidencial.Name = "tbTelResidencial";
+            this.tbTelResidencial.Size = new System.Drawing.Size(229, 20);
+            this.tbTelResidencial.TabIndex = 84;
+            // 
+            // tbTelCelular
+            // 
+            this.tbTelCelular.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbTelCelular.Location = new System.Drawing.Point(367, 131);
+            this.tbTelCelular.Name = "tbTelCelular";
+            this.tbTelCelular.Size = new System.Drawing.Size(229, 20);
+            this.tbTelCelular.TabIndex = 83;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbNombre.Location = new System.Drawing.Point(367, 86);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(229, 20);
+            this.tbNombre.TabIndex = 81;
+            // 
+            // bttAceptar
+            // 
+            this.bttAceptar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bttAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(25)))), ((int)(((byte)(42)))));
+            this.bttAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttAceptar.ForeColor = System.Drawing.Color.LightGray;
+            this.bttAceptar.Location = new System.Drawing.Point(518, 362);
+            this.bttAceptar.Margin = new System.Windows.Forms.Padding(4);
+            this.bttAceptar.Name = "bttAceptar";
+            this.bttAceptar.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.bttAceptar.Size = new System.Drawing.Size(78, 30);
+            this.bttAceptar.TabIndex = 80;
+            this.bttAceptar.Text = "Aceptar";
+            this.bttAceptar.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.LightGray;
+            this.label7.Location = new System.Drawing.Point(231, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(124, 20);
+            this.label7.TabIndex = 78;
+            this.label7.Text = "Departamento";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.LightGray;
+            this.label6.Location = new System.Drawing.Point(173, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(182, 20);
+            this.label6.TabIndex = 77;
+            this.label6.Text = "Dirección Residencial";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.LightGray;
+            this.label5.Location = new System.Drawing.Point(178, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 20);
+            this.label5.TabIndex = 76;
+            this.label5.Text = "Teléfono Residencial";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LightGray;
+            this.label4.Location = new System.Drawing.Point(215, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 20);
+            this.label4.TabIndex = 75;
+            this.label4.Text = "Teléfono Celular";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(284, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 73;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(14, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 24);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Ingrese los datos que se solicitan a continuación";
+            // 
+            // labeRE
+            // 
+            this.labeRE.AutoSize = true;
+            this.labeRE.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeRE.ForeColor = System.Drawing.Color.LightGray;
+            this.labeRE.Location = new System.Drawing.Point(13, 11);
+            this.labeRE.Name = "labeRE";
+            this.labeRE.Size = new System.Drawing.Size(318, 25);
+            this.labeRE.TabIndex = 71;
+            this.labeRE.Text = "Editar datos de un proveedor";
+            // 
+            // Actualizacion_proveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.bttClose);
+            this.Controls.Add(this.tbCorreo);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.cbDep);
+            this.Controls.Add(this.tbDirRes);
+            this.Controls.Add(this.tbTelResidencial);
+            this.Controls.Add(this.tbTelCelular);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.bttAceptar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labeRE);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Actualizacion_proveedores";
+            this.Text = "Actualizacion_proveedores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttClose;
+        private System.Windows.Forms.TextBox tbCorreo;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox cbDep;
+        private System.Windows.Forms.TextBox tbDirRes;
+        private System.Windows.Forms.TextBox tbTelResidencial;
+        private System.Windows.Forms.TextBox tbTelCelular;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button bttAceptar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labeRE;
+    }
+}
